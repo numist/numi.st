7dd76c4bbe2feeaca0d68603bdb00807b9b3f273
Scott Perry <dev@numist.net>
2022-11-09 17:17:00 -0800

    Borrow heavily from jekyll-last-modified-at and its open pull requests

--- _plugins/undated_posts.rb
+++ _plugins/undated_posts.rb
@@ -1,3 +1,5 @@
+require 'posix/spawn'
+
 module UndatedPosts
   # Add dates to everything
   class Date
@@ -6,13 +8,11 @@ module UndatedPosts
         @@git = Git.new(site)
       end
       
-      def inject_dates
-        proc { |page|
-          # detect when we've got undated document injection working
-          raise "nil date for document" if page.data['date'].nil? and page.instance_of? Jekyll::Document
-          page.data['date'] = @@git.files[page.path]&[:last_created_at] || "1969-12-31" if page.data['date'].nil?
-          page.data['modified_at'] = @@git.files[page.path]&[:last_modified_at] || Time.now()
-        }
+      def inject_dates(page)
+        # detect when we've got undated document injection working
+        raise "nil date for document" if page.data['date'].nil? and page.instance_of? Jekyll::Document
+        page.data['date'] = @@git.files.dig(page.path, :last_created_at) || "1969-12-31" if page.data['date'].nil?
+        page.data['modified_at'] = @@git.files.dig(page.path, :last_modified_at) || Time.now()
       end
     end
 
@@ -30,11 +30,88 @@ module UndatedPosts
   class Git
     attr_reader :files
     
+    def top_level_directory
+      return nil unless git_repo?
+
+      @top_level_directory ||= begin
+        Dir.chdir(@site_source) do
+          @top_level_directory = File.join(Executor.sh('git', 'rev-parse', '--show-toplevel'), '.git')
+        end
+      # is this rescue actually necessary? looks like it's handled in Executor…
+      rescue StandardError
+        nil
+      end
+    end
+    
+    def git_repo?
+      @is_git_repo ||= begin
+        Dir.chdir(@site_source) do
+          Executor.sh('git', 'rev-parse', '--is-inside-work-tree').eql? 'true'
+        end
+      # is this rescue actually necessary? looks like it's handled in Executor…
+      rescue StandardError
+        false
+      end
+    end
+    
+    
     def initialize(site)
-      Jekyll.logger.debug "jekyll-times: Initializing git cache"
-      @site_path = site.source
+      Jekyll.logger.debug "         Resetting: git cache"
+      @site_source = site.source
       @files = {}
-      # raise "#{@site_path}"
+      
+      lines = Executor.sh(
+        'git',
+        '--git-dir',
+        top_level_directory,
+        'log',
+        '--name-only',
+        '--date=unix',
+        '--pretty=%%these-files-modified-at:%ct'
+      ).split("\n")
+
+      timestamp = nil
+      lines.each do |line|
+        case
+        when line.empty? # skip
+        when line.start_with?('%these-files-modified-at:')
+          timestamp = line.split(':')[1]
+        else
+          @files[line] = { last_modified_at: timestamp } unless @files.key?(line)
+          @files[line][:last_created_at] = timestamp
+        end
+      end
+    end
+  end
+  
+  module Executor
+    extend POSIX::Spawn
+
+    def self.sh(*args)
+      r, w = IO.pipe
+      e, eo = IO.pipe
+      pid = spawn(
+        *args,
+        :out => w, r => :close,
+        :err => eo, e => :close
+      )
+
+      if pid.positive?
+        w.close
+        eo.close
+        out = r.read
+        err = e.read
+        ::Process.waitpid(pid)
+        "#{out} #{err}".strip if out
+      end
+    ensure
+      [r, w, e, eo].each do |io|
+        begin
+          io.close
+        rescue StandardError
+          nil
+        end
+      end
     end
   end
 
@@ -53,8 +130,12 @@ module UndatedPosts
   #
 
   # Inject dates into all pages and documents
-  Jekyll::Hooks.register :pages, :post_init, &Date.inject_dates
-  Jekyll::Hooks.register :documents, :pre_render, &Date.inject_dates
+  Jekyll::Hooks.register :pages, :post_init do |page|
+    Date.inject_dates(page)
+  end
+  Jekyll::Hooks.register :documents, :pre_render do |page|
+    Date.inject_dates(page)
+  end
   Jekyll::Hooks.register :site, :after_reset do |site|
     Date.git_init(site)
   end
