c573aa4e85ea974af6ece660efab3895c62c0b8c
Scott Perry <dev@numist.net>
2023-03-16 22:18:41 -0700

    use the digraph id for the svg tag's id

--- _plugins/graphviz.rb
+++ _plugins/graphviz.rb
@@ -1,23 +1,24 @@
-require 'digest/md5'
 require 'ruby-graphviz'
 
 module Jekyll
   class GraphvizBlock < Liquid::Block
-    def initialize(tag_name, markup, tokens)
+    def initialize(tag_name, markup, options)
       super
-      @markup = markup.strip
+      @tag = markup
     end
 
     def render(context)
-      # Generate a unique ID for the rendered graph
-      graph_id = Digest::MD5.hexdigest(super)
+      graph_src = super
 
       # Parse the Graphviz DOT input
-      graph = GraphViz.parse_string(super)
-      raise "not a valid graph definition" unless graph
+      # raise "#{@tag}" unless @tag.empty?
+      # raise "#{@options}" unless @options.nil?
+      graph = GraphViz.parse_string(graph_src)
+      raise "\"#{graph_src.lines.first.chomp}\" is not a valid graph definition" unless graph
+      raise "graph is not named?" unless graph.id
 
       # Render the graph as SVG output and add a unique ID to the <svg> element
-      svg_output = graph.output(:svg => String).sub(/^.*<svg/m, "<svg id=\"graphviz-#{graph_id}\"")
+      svg_output = graph.output(:svg => String).sub(/^.*<svg/m, "<svg id=\"graphviz-#{ graph.id }\"")
       
       # Wrap the SVG in a <div> element with a class of "graphviz"
       "<div class=\"graphviz\">#{svg_output.to_s}</div>"
