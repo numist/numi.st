d34240a41c15db65c3ad9f558ce519693ca2b492
Scott Perry <dev@numist.net>
2023-03-20 22:38:05 -0700

    Filter out white background on graphs, the site's styles already provide a background

--- _plugins/graphviz.rb
+++ _plugins/graphviz.rb
@@ -14,12 +14,13 @@ module Jekyll
       graph_id = @tag unless @tag.empty?
       graph_id ||= graph.id if graph.id.match?(/^[a-zA-Z0-9]+$/)
       graph_id ||= Digest::MD5.hexdigest(super)
-      
+
       svg = graph.output(:svg => String).
-        sub(/^.*<svg/m, "<svg class=\"graphviz\" id=\"#{ graph_id }\"").
-        gsub(' font-family="Times,serif"', ' font-family="Museo"').
-        gsub(' font-family="Courier,monospace"', ' font-family="Fira Code"').
+        gsub(' font-family="Times,serif"',          ' font-family="Museo"').
+        gsub(' font-family="Courier,monospace"',    ' font-family="Fira Code"').
         gsub(' font-family="Helvetica,sans-Serif"', ' font-family="Fira Sans"').
+        sub(/^.*?<svg/m,                            "<svg class=\"graphviz\" id=\"#{ graph_id }\"").
+        sub(/<polygon fill="white" stroke="none" .*?>/m, '').
         gsub(/<!--.*?-->/m, '')
 
       "#{svg}"
